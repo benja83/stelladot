cd2a1961fe64b23bd51996b8606b31b6059c84ce
benja83 <tarennebenjamin@gmail.com>
2014-10-30 17:18:29 +0100

    added validation test for data_type inclusion

--- spec/models/config_setting_spec.rb
+++ spec/models/config_setting_spec.rb
@@ -6,18 +6,26 @@ RSpec.describe ConfigSetting, :type => :model do
       setting = ConfigSetting.new
       expect(setting.valid?).to equal(false)
     end
+
     it "don't create a setting without type" do
       setting_without_type = ConfigSetting.new name: "email"
       expect(setting_without_type.valid?).to equal(false)
     end
+
     it "don't create a setting without name" do
       setting_without_name = ConfigSetting.new data_type: "string"
       expect(setting_without_name.valid?).to equal(false)
     end
+
     it "don't create a setting with the same name" do
       ConfigSetting.create! name: "email",data_type: "string"
       setting_not_unique = ConfigSetting.new name: "email",data_type: "string"
       expect(setting_not_unique.valid?).to eql(false)
+    end
+
+    it "don't create a setting with a data_type not include in (string integer float boolean)" do
+      setting_with_data_type_not_include = ConfigSetting.new name: "email",data_type: "magic"
+      expect(setting_with_data_type_not_include.valid?).to equal(false)
     end
   end
 end
