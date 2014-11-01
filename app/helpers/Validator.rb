2ddd8779eaa58127a59d3009f503dba0521b00c9
benja83 <tarennebenjamin@gmail.com>
2014-11-01 14:56:02 +0100

    refactor configsetting

--- app/helpers/Validator.rb
+++ app/helpers/Validator.rb
@@ -2,4 +2,15 @@ class Validator
   def initialize(data_type)
     @data_type = data_type
   end
+
+  def check(value)
+    get_type(value) == @data_type || (get_type(value) == "integer" && @data_type == "float")
+  end
+
+  def get_type(value)
+    return "integer" if /\A[0-9]*\z/ === value
+    return "float" if /\A[0-9]*\.[0-9]*\z/ === value
+    return "boolean" if "true" == value || "false" == value
+    "string"
+  end
 end
--- app/models/config_setting.rb
+++ app/models/config_setting.rb
@@ -2,44 +2,25 @@ require_relative "../helpers/Validator.rb"
 
 class ConfigSetting < ActiveRecord::Base
 
-  attr_accessor :value,:data_type,:validators
+  after_initialize :create_validators
 
-  after_initialize :set_instance_variable,:set_class_variable,:create_validators
-
-  def set_instance_variable
-    @data_type_available = %w(string integer float boolean)
-  end
-
-  def set_class_variable
-    @@validators = {}
-  end
+  @@data_type_available = %w(string integer float boolean)
+  @@validators = {}
 
   def create_validators
-    @data_type_available.each do |element|
+    @@data_type_available.each do |element|
       @@validators[element] = Validator.new(element) unless @@validators[element]
     end
   end
 
   validates :name, presence: true, uniqueness: true
-  validates :data_type, presence: true, inclusion: { in: %w(string integer float boolean), message: "%{value} is not a valid data_type" }
+  validates :data_type, presence: true, inclusion: { in: @@data_type_available, message: "%{value} is not a valid data_type" }
   validate :type_value_match_data_type
 
   def type_value_match_data_type
-    if self.data_type != "float"
-      if self.get_type_value != self.data_type
-        errors.add(:value, "don't have the same #{self.data_type} of the setting")
-      end
-    elsif self.get_type_value != "float" && self.get_type_value != "integer"
+    if errors.blank? && !@@validators[self.data_type].check(self.value)
       errors.add(:value, "don't have the same #{self.data_type} of the setting")
     end
   end
-
-  def get_type_value
-    return "integer" if /\A[0-9]*\z/ === self.value
-    return "float" if /\A[0-9]*\.[0-9]*\z/ === self.value
-    return "boolean" if "true" == self.value || "false" == self.value
-    "string"
-  end
-
 
 end
